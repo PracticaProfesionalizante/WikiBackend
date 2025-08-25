f6834d102a9ead537403b726f161296808671b46
NT-0286-CBA\Teclab1 <a>
2025-08-25 12:28:44 -0300

    Inicio Proyecto

---
+++ src/test/java/com/teclab/practicas/WikiBackend/WikiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teclab.practicas.WikiBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
