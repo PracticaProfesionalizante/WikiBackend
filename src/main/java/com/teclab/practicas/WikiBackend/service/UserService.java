c9a95e029f390d8f1159614620f1f7f2fe86b338
Erich Schnell <erich2128@gmail.com>
2025-09-02 10:56:48 -0300

    Agregamos UserRepository

---
+++ src/main/java/com/teclab/practicas/WikiBackend/service/UserService.java
@@ -0,0 +1,36 @@
+package com.teclab.practicas.WikiBackend.service;
+
+import com.teclab.practicas.WikiBackend.entity.User;
+import com.teclab.practicas.WikiBackend.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles().stream()
+                    .map(role -> new SimpleGrantedAuthority(role.getName()))
+                    .collect(Collectors.toList()));
+    }
+}
