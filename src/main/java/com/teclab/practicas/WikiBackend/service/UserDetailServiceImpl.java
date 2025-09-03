cbab13251271cf3ebdd3084f4149e91a15294267
Erich Schnell <erich2128@gmail.com>
2025-09-03 15:40:01 -0300

    fix: Hice un cambio de nombre a UserDetailServiceImpl

---
+++ src/main/java/com/teclab/practicas/WikiBackend/service/UserDetailServiceImpl.java
@@ -0,0 +1,35 @@
+package com.teclab.practicas.WikiBackend.service;
+
+import com.teclab.practicas.WikiBackend.entity.User;
+import com.teclab.practicas.WikiBackend.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.stream.Collectors;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles().stream()
+                    .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                    .collect(Collectors.toList()));
+    }
+}
