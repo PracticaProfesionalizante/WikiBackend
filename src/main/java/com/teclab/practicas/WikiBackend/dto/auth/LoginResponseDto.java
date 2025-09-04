905914253f7c96b2568484772cbd74a17104618e
Erich Schnell <erich2128@gmail.com>
2025-09-04 10:37:16 -0300

    feat: se agregaron los DTOs de respuestas de Login y RefreshToken

---
+++ src/main/java/com/teclab/practicas/WikiBackend/dto/auth/LoginResponseDto.java
@@ -0,0 +1,12 @@
+package com.teclab.practicas.WikiBackend.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponseDto {
+    private String accessToken;
+}
