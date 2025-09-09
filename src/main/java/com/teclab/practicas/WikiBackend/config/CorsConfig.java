25ca11277e071003e30e895ef2e08d37f9a1c45d
Erich Schnell <erich2128@gmail.com>
2025-09-09 16:50:45 -0300

    fix: llamada de cors en security y se quito una barra en el dns publico del front en la configuracion del cors

--- src/main/java/com/teclab/practicas/WikiBackend/config/CorsConfig.java
+++ src/main/java/com/teclab/practicas/WikiBackend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://practicas.teclab.edu.ar/")); // URL de tu frontend de Vue
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://practicas.teclab.edu.ar")); // URL de tu frontend de Vue
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
--- src/main/java/com/teclab/practicas/WikiBackend/config/SecurityConfig.java
+++ src/main/java/com/teclab/practicas/WikiBackend/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,8 +15,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableMethodSecurity // Permite seguridad a nivel de método con @PreAuthorize
@@ -54,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .cors(withDefaults()) // Utiliza la configuración por defecto de tu CorsConfig
+                .cors(Customizer.withDefaults()) // Utiliza la configuración por defecto de tu CorsConfig
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
