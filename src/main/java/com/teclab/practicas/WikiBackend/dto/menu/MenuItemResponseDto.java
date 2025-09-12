a756deb0d0360a7c03b2a304bcae0f66832a4755
Agustina Varela <agusvarela2002@hotmail.com>
2025-09-12 14:34:41 -0300

    feat : Agrege controller, Dto de respuesta y request del Menu desplegable

---
+++ src/main/java/com/teclab/practicas/WikiBackend/dto/menu/MenuItemResponseDto.java
@@ -0,0 +1,24 @@
+package com.teclab.practicas.WikiBackend.dto.menu;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class MenuItemResponseDto {
+
+        private Long id;
+        private String name;
+        private String path;
+        private String icon;
+        private Integer displayOrder;
+        private List<MenuItemResponseDto> children;     // Para la jerarquía de submenús
+        private Long parent; 
+        
+        
+}
