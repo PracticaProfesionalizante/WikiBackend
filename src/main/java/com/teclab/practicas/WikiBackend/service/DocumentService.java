1d131a19e38a614b8663d66024cb9d54827a59ca
Agustina Varela <agusvarela2002@hotmail.com>
2025-09-23 16:16:10 -0300

    docs: se arreglaron nombres de la documentación en roles, service y repository

--- src/main/java/com/teclab/practicas/WikiBackend/service/DocumentService.java
+++ src/main/java/com/teclab/practicas/WikiBackend/service/DocumentService.java
@@ -1,18 +1,16 @@
 package com.teclab.practicas.WikiBackend.service;
 
+import com.teclab.practicas.WikiBackend.dto.documents.DocumentDetailResponseDto;
+import com.teclab.practicas.WikiBackend.dto.documents.DocumentUrlRequestDto;
 import com.teclab.practicas.WikiBackend.entity.Document;
 
 import java.util.List;
 
 public interface DocumentService {
 
-    public Document createDocument(Document document);
-    public List<Document> getAllDocuments();
+    public Document createDocument(DocumentUrlRequestDto requestDto);
+    public List<DocumentDetailResponseDto> getAllDocuments();
     public Document getDocumentByRoles(Long id); //si es correcto devolver documento sino devolver error
-    public Document updateDocument(Long id, Document updatedDocument);
+    public Document updateDocument(Long id, DocumentUrlRequestDto requestDto);
     public void deleteDocument(Long id);
 }
-
-
-// DocumentUrlRequestDto
-// DocumentDetailResponseDto
