3d74299e1ec5c71ccf3a1220f3b47197230ca506
Erich Schnell <erich2128@gmail.com>
2025-09-26 14:40:26 -0300

    fix: se mejoro la exception de FileStorageException

--- src/main/java/com/teclab/practicas/WikiBackend/exception/FileStorageException.java
+++ src/main/java/com/teclab/practicas/WikiBackend/exception/FileStorageException.java
@@ -1,9 +1,15 @@
 package com.teclab.practicas.WikiBackend.exception;
 
-import java.io.IOException;
-
 public class FileStorageException extends RuntimeException {
-    public FileStorageException(String message, IOException ex) {
-        super(message + ex.getMessage());
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public FileStorageException(Throwable cause) {
+        super(cause);
     }
 }
